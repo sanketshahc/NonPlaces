01d4864588753d9edb2a86bde4b3bda540df5a5a
sanketshahc <sanket.shah.c@gmail.com>
2020-11-24 19:58:47 -0500

    ass 3

--- routes.py
+++ routes.py
@@ -1,8 +1,9 @@
 import csv
 import requests
+import numpy
 
 # opening geojson file for writing
-r = open('routes.geojson', 'a', encoding='utf8')
+r = open('test_paths.geojson', 'a', encoding='utf8')
 start = """\
 {
     "type": "FeatureCollection",
@@ -16,7 +17,7 @@ r.write(start)
 
 def router():
         # opening data file...returns a file object which is a buffered stream
-    with open('dataset/taxi_data.csv', 'r') as f:
+    with open('test.csv', 'r') as f:
         # DictReader returns a reader object, which is iterable by row. each row is mapped to dict.
         data = csv.DictReader(f)
         # chunk = (next(data) for i in range(3)) # chunk for testing
@@ -48,6 +49,20 @@ def router():
                 r.close()
                 return
 
+            # build list of ratios for timestamping
+            norm = numpy.linalg.norm
+            dist = res.json()['routes'][0]['distance']
+            ways = res.json()['routes'][0]['geometry']['coordinates']
+            euclideans = numpy.zeros(len(ways))
+            for i, w in enumerate(ways):
+
+            euclidean_sum = [norm(x - ways[w+1]) for w, x in enumerate(ways)]
+            ratios = [norm(x - ways[w+1])/dist for w, x in enumerate(ways)]
+
+            for i, j in ways:
+                j += [0, ratios[i] * dist ]
+
+            route = res.json()['routes'][0]['duration']
             route = res.json()['routes'][0]['geometry']
             route_geo = str({"geometry": route})
             route_geo = route_geo.replace("'",'"')
@@ -62,4 +77,24 @@ def router():
 router()
 r.write(end)
 r.close()
-
+#
+# Model format geojson""
+# {
+#   "type": "FeatureCollection",
+#   "features": [
+#     {
+#       "type": "Feature",
+#       "properties": {
+#         "vendor":  "A"
+#       },
+#       "geometry": {
+#         "type": "LineString",
+#         "coordinates": [
+#           [-74.20986, 40.81773, 0, 1564184363],
+#           [-74.20987, 40.81765, 0, 1564184396],
+#           [-74.20998, 40.81746, 0, 1564184409]
+#         ]
+#       }
+#     }
+#   ]
+# }
